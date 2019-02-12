3af14cb3416c4a60747d015330f444b8a9509b64
dimotim <datdimotim@gmail.com>
2019-02-12 14:54:07 +0300

    swing combobox

--- src/main/java/com/dimotim/opengl_lab/BasicFrame.java
+++ src/main/java/com/dimotim/opengl_lab/BasicFrame.java
@@ -4,69 +4,108 @@ package com.dimotim.opengl_lab;
 
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.Animator;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class BasicFrame implements GLEventListener {
+    private static int primitiveToGLConstant(ControlPanel.Primitive primitive){
+        switch (primitive){
+            case GL_LINES: return GL2.GL_LINES;
+            case GL_QUADS: return GL2.GL_QUADS;
+            case GL_LINE_LOOP: return GL2.GL_LINE_LOOP;
+            case GL_POINTS: return GL2.GL_POINTS;
+            case GL_POLYGON: return GL2.GL_POLYGON;
+            case GL_TRIANGLES: return GL2.GL_TRIANGLES;
+            case GL_LINE_STRIP: return GL2.GL_LINE_STRIP;
+            case GL_QUAD_STRIP:return GL2.GL_QUAD_STRIP;
+            case GL_TRIANGLE_FAN: return GL2.GL_TRIANGLE_FAN;
+            case GL_TRIANGLE_STRIP:return GL2.GL_TRIANGLE_STRIP;
+            default:throw new RuntimeException();
+        }
+    }
+
+    private ControlPanel.Primitive primitive= ControlPanel.Primitive.GL_LINES;
+
+    public void setPrimitive(ControlPanel.Primitive primitive){
+        this.primitive=primitive;
+    }
 
     public void display(GLAutoDrawable drawable) {
-
         final GL2 gl = drawable.getGL().getGL2();
-        gl.glBegin (GL2.GL_LINES);
 
-        //drawing the base
-        gl.glBegin (GL2.GL_LINES);
-        gl.glVertex3f(-0.50f, -0.50f, 0);
-        gl.glVertex3f(0.50f, -0.50f, 0);
-        gl.glEnd();
+        gl.glBegin(primitiveToGLConstant(primitive));
+            gl.glVertex3f(-0.50f, -0.50f, 0);
+            gl.glVertex3f(0.50f, -0.50f, 0);
+            gl.glVertex3f(0f, 0.50f, 0);
+            gl.glVertex3f(-0.50f, -0.50f, 0);
+            gl.glVertex3f(0f, 0.50f, 0);
+            gl.glVertex3f(0.50f, -0.50f, 0);
 
-        //drawing the right edge
-        gl.glBegin (GL2.GL_LINES);
-        gl.glVertex3f(0f, 0.50f, 0);
-        gl.glVertex3f(-0.50f, -0.50f, 0);
-        gl.glEnd();
-
-        //drawing the lft edge
-        gl.glBegin (GL2.GL_LINES);
-        gl.glVertex3f(0f, 0.50f, 0);
-        gl.glVertex3f(0.50f, -0.50f, 0);
         gl.glEnd();
         gl.glFlush();
-
+        System.out.println("www");
     }
 
     public void dispose(GLAutoDrawable arg0) {
-        //method body
+
     }
 
     public void init(GLAutoDrawable arg0) {
-        // method body
+
     }
 
     public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
-        // method body
+
     }
 
     public static void main(String[] args) {
-
-        //getting the capabilities object of GL2 profile
         final GLProfile profile = GLProfile.get(GLProfile.GL2);
         GLCapabilities capabilities = new GLCapabilities(profile);
 
-        // The canvas
         final GLCanvas glcanvas = new GLCanvas(capabilities);
-        BasicFrame b = new BasicFrame();
-        glcanvas.addGLEventListener(b);
-        glcanvas.setSize(400, 400);
 
-        //creating frame
-        final JFrame frame = new JFrame (" Basic Frame");
+        Animator animator=new Animator(glcanvas);
+        animator.start();
 
-        //adding canvas to it
-        frame.getContentPane().add(glcanvas);
-        frame.setSize(frame.getContentPane().getPreferredSize());
-        frame.setVisible(true);
+        BasicFrame frame = new BasicFrame();
+        glcanvas.addGLEventListener(frame);
+        glcanvas.setSize(700, 700);
 
-    }//end of main
+        JPanel content=new JPanel();
+        content.add(glcanvas);
+        content.add(new ControlPanel(frame,content));
 
-}//end of classimport
+        final JFrame window = new JFrame ("Basic Frame");
+
+
+        window.setContentPane(content);
+        window.setSize(window.getContentPane().getPreferredSize());
+        window.setVisible(true);
+
+    }
+
+}
+
+class ControlPanel extends JPanel{
+    ControlPanel(BasicFrame frame, JPanel parent){
+        JComboBox<Primitive> comboBox=new JComboBox<>(Primitive.values());
+        add(comboBox);
+        comboBox.addItemListener(v->{
+            frame.setPrimitive((Primitive) comboBox.getSelectedItem());
+            parent.updateUI();
+        });
+    }
+    enum Primitive{
+        GL_POINTS,
+        GL_LINES,
+        GL_LINE_STRIP,
+        GL_LINE_LOOP,
+        GL_TRIANGLES,
+        GL_TRIANGLE_STRIP,
+        GL_TRIANGLE_FAN,
+        GL_QUADS,
+        GL_QUAD_STRIP,
+        GL_POLYGON
+    }
+}
