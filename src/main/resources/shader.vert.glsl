3353150cc27739fcbae1ef8ffb6e7ffd344b547e
dimotim <datdimotim@gmail.com>
2019-03-28 23:28:55 +0300

    баг с пересчетом нормали в шейдере починен

--- src/main/resources/shader.vert.glsl
+++ src/main/resources/shader.vert.glsl
@@ -11,9 +11,9 @@ varying vec3 normal;
 void main(){
     ap_Color=a_Color;
     v_Tex_Coord=a_Tex_Coord;
-    vec4 n4=vec4(a_normal,1);
+    vec4 n4=vec4(a_normal,0);
     vec4 nn=u_Matrix*u_Model_Matrix*n4;
-    normal = vec3(nn[0]/nn[3],nn[1]/nn[3],nn[2]/nn[3]);
+    normal = vec3(nn[0],nn[1],nn[2]);
     //normal=vec3(1,1,0);
     gl_Position =u_Monitor_Matrix*u_Matrix*u_Model_Matrix*a_Position;
 }
