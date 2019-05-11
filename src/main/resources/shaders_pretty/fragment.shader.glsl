5641c2d2710a55295a53063dda255ae24a98c232
dimotim <datdimotim@gmail.com>
2019-05-11 17:54:56 +0300

    отсечение зеркального отважения в темноте

--- src/main/resources/shaders_pretty/fragment.shader.glsl
+++ src/main/resources/shaders_pretty/fragment.shader.glsl
@@ -19,15 +19,14 @@ out vec4 outColor;
 void main(){
     float onePixel =1.0 / 512;
 
-    vec4 light_mToPosition=position-light_m;
-    vec3 light=normalize(vec3(light_mToPosition[0],light_mToPosition[1],light_mToPosition[2]));
+    vec3 ptTolight=normalize((position-light_m).xyz);
+    vec3 ptToEye=normalize(position.xyz-observer);
 
     vec4 textureColor=texture(u_texture, v_Tex_Coord1);
 
-    vec3 uvt=vec3(0,0,1.0);
     float dx=texture(u_texture, v_Tex_Coord1+vec2(onePixel,0)).x-texture(u_texture, v_Tex_Coord1).x;
     float dy=texture(u_texture, v_Tex_Coord1+vec2(0,onePixel)).x-texture(u_texture, v_Tex_Coord1).x;
-    uvt=normalize(vec3(dx,dy,0.7));
+    vec3 uvt=normalize(vec3(dx,dy,0.7));
 
     mat3 mtex=mat3(t1Vec,t2Vec,vec3(0,0,1.0));
     mat3 mvert=mat3(uVec,vVec,normal);
@@ -35,9 +34,12 @@ void main(){
 
     vec3 resNormal=normal*(1-imgNormalRatio)+exactNormal;
 
-    vec4 ambientColor=vec4(Color1,1)+textureColor*(1-imgNormalRatio);
-    vec4 diffuseColor=(vec4(Color1,1)+textureColor*(1-imgNormalRatio))*max(dot(-light,resNormal),0);
-    vec4 specularColor=vec4(1,1,1,1)*pow(max(dot(normalize(position.xyz-observer),-normal),0),100);
+    float specIsPresent=0;
+    if(dot(ptToEye,ptTolight)>0)specIsPresent=1;
 
-    outColor=ambientColor*0.2+0.5*diffuseColor+specularColor*0.1;
+    vec4 ambientColor=vec4(Color1,1)+textureColor*(1-imgNormalRatio);
+    vec4 diffuseColor=(vec4(Color1,1)+textureColor*(1-imgNormalRatio))*max(dot(-ptTolight,resNormal),0);
+    vec4 specularColor=specIsPresent*vec4(1,1,1,1)*pow(max(dot(ptToEye,-normal),0),100);
+
+    outColor=ambientColor*0.2+0.5*diffuseColor+specularColor*0.2;
 }
